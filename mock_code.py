d3b9c7945218ff7aa489af9886d98215fc623a7e
Kleyton Torikai <kleyton.torikai@gmail.com>
2020-02-03 19:28:28 -0300

    Updated .gitignore

--- mock_code.py
+++ mock_code.py
@@ -2,7 +2,7 @@ from request_glassifier import request_img_glassifier
 
 
 api_url = 'https://glassifier-api.herokuapp.com/api/glassifier'
-my_img_path = 'img/cat.jpg'
+my_img_path = 'img/milu.jpg'
 filtered_image = request_img_glassifier(my_img_path, api_url)
-filtered_image.save('output/filtered_cat.jpg')
+filtered_image.save('output/filtered_milu.jpg')
 filtered_image.show()
